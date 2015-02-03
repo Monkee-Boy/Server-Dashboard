0ef6d6d87d24ab99d4013ecb48f06947bdd447e1
John Hoover <defvayne23@gmail.com>
2015-02-02 21:39:11 -0600

    Add chart cache.

--- app/controllers/LinodeController.php
+++ app/controllers/LinodeController.php
@@ -23,65 +23,91 @@ class LinodeController extends BaseController {
   */
   public function bandwidth()
   {
-    $request = $this->call_linode('account.info');
+    $cache_key = 'linode_bandwidth';
 
-    $value = $request['DATA']['TRANSFER_USED'];
-    if($value >= 8388608)
+    if (Cache::has($cache_key))
     {
-      $value_label = round($value/1024,2).'TB';
+      $aData = Cache::get($cache_key);
     }
     else
     {
-      $value_label = $value.'GB';
+      $request = $this->call_linode('account.info');
+
+      $value = $request['DATA']['TRANSFER_USED'];
+      if($value >= 8388608)
+      {
+        $value_label = round($value/1024,2).'TB';
+      }
+      else
+      {
+        $value_label = $value.'GB';
+      }
+
+      $aData = [
+        'min'=>0,
+        'max'=>$request['DATA']['TRANSFER_POOL'],
+        'suffix'=>'GB',
+        'value'=>$value,
+        'max_label' => round($request['DATA']['TRANSFER_POOL']/1024,2).'TB',
+        'value_label' => $value_label
+      ];
+
+      $expiresAt = new DateTime();
+      $expiresAt = $expiresAt->modify('+1 Hour');
+      Cache::put($cache_key, $aData, $expiresAt);
     }
 
-    $data = [
-      'min'=>0,
-      'max'=>$request['DATA']['TRANSFER_POOL'],
-      'suffix'=>'GB',
-      'value'=>$value,
-      'max_label' => round($request['DATA']['TRANSFER_POOL']/1024,2).'TB',
-      'value_label' => $value_label
-    ];
-
-    return Response::json($data, 200);
+    return Response::json($aData, 200);
   }
 
   public function storage()
   {
-    $request = $this->call_linode('linode.disk.list', array('LinodeID'=>828311,'DiskID'=>2821695));
+    $cache_key = 'linode_storage';
 
-    $disk_total = disk_total_space('/');
-    $disk_free = disk_free_space('/');
-
-    $value = ($disk_total - $disk_free);
-    if($value >= 1073741824)
+    if (Cache::has($cache_key))
     {
-      $value_label = round($value/1073741824,2).'GB';
-    }
-    elseif($value >= 1048576)
-    {
-      $value_label = round($value/1048576,2).'MB';
-    }
-    elseif($value >= 1024)
-    {
-      $value_label = round($value/1024,2).'KB';
+      $aData = Cache::get($cache_key);
     }
     else
     {
-      $value_label = $value.'B';
+      $request = $this->call_linode('linode.disk.list', array('LinodeID'=>828311,'DiskID'=>2821695));
+
+      $disk_total = disk_total_space('/');
+      $disk_free = disk_free_space('/');
+
+      $value = ($disk_total - $disk_free);
+      if($value >= 1073741824)
+      {
+        $value_label = round($value/1073741824,2).'GB';
+      }
+      elseif($value >= 1048576)
+      {
+        $value_label = round($value/1048576,2).'MB';
+      }
+      elseif($value >= 1024)
+      {
+        $value_label = round($value/1024,2).'KB';
+      }
+      else
+      {
+        $value_label = $value.'B';
+      }
+
+      $aData = [
+        'min'=>0,
+        'max'=>$request['DATA'][0]['SIZE'],
+        'suffix'=>'MB',
+        'value'=>round($value/1048576,2),
+        'max_label' => round($request['DATA'][0]['SIZE']/1024,2).'GB',
+        'value_label' => $value_label
+      ];
+
+      $expiresAt = new DateTime();
+      $expiresAt = $expiresAt->modify('+1 Hour');
+      Cache::put($cache_key, $aData, $expiresAt);
     }
 
-    $data = [
-      'min'=>0,
-      'max'=>$request['DATA'][0]['SIZE'],
-      'suffix'=>'MB',
-      'value'=>round($value/1048576,2),
-      'max_label' => round($request['DATA'][0]['SIZE']/1024,2).'GB',
-      'value_label' => $value_label
-    ];
-
-    return Response::json($data, 200);
+    return Response::json($aData, 200);
   }
 
   protected function call_linode($request, $arguments=array())
--- app/controllers/OvertimeController.php
+++ app/controllers/OvertimeController.php
@@ -7,7 +7,76 @@ class OvertimeController extends BaseController {
   */
   public function bandwidth($domain_name, $subdomain_name = null)
   {
-    $oBandwidth = Bandwidth::select('request_time', DB::raw('sum(bytes_received) as bytes_received'), DB::raw('sum(bytes_sent) as bytes_sent'))
+    $cache_key = 'overtime_bandwidth_'.$domain_name.'_'.$subdomain_name;
+
+    if (Cache::has($cache_key))
+    {
+      $aData = Cache::get($cache_key);
+    }
+    else
+    {
+      $oBandwidth = Bandwidth::select('request_time', DB::raw('sum(bytes_received) as bytes_received'), DB::raw('sum(bytes_sent) as bytes_sent'))
+        ->whereHas('domain', function($q) use ($domain_name)
+        {
+          $q->where('domain', $domain_name);
+        })
+        ->groupBy(DB::raw('YEAR(request_time), MONTH(request_time), DAY(request_time)'))
+        ->orderBy('request_time');
+
+      if(!empty($subdomain_name)) {
+        $oBandwidth = $oBandwidth->whereHas('domain', function($q) use ($subdomain_name)
+        {
+          $q->where('subdomain', $subdomain_name);
+        });
+      }
+
+      $aBandwidth = $oBandwidth->get();
+
+      $aReceived = array(
+        'name' => 'Received',
+        'data' => array(),
+        'tooltip' => array(
+          'valueDecimals' => 2,
+          'valueSuffix' => ' KB'
+        )
+      );
+      $aSent = array(
+        'name' => 'Sent',
+        'data' => array(),
+        'tooltip' => array(
+          'valueDecimals' => 2,
+          'valueSuffix' => ' KB'
+        )
+      );
+
+      foreach($aBandwidth as $aRow) {
+        $date = new DateTime($aRow['request_time']);
+
+        $aReceived['data'][] = array((int)$date->format('U')*1000, (float)number_format($aRow['bytes_received'] / 1024, 2));
+        $aSent['data'][] = array((int)$date->format('U')*1000, (float)number_format($aRow['bytes_sent'] / 1024, 2));
+      }
+
+      $aData = array($aReceived, $aSent);
+
+      $expiresAt = new DateTime();
+      $expiresAt = $expiresAt->modify('+1 Hour');
+      Cache::put($cache_key, $aData, $expiresAt);
+    }
+
+    return Response::json($aData, 200);
+  }
+
+  public function time_taken($domain_name, $subdomain_name = null)
+  {
+    $cache_key = 'overtime_timetaken_'.$domain_name.'_'.$subdomain_name;
+
+    if (Cache::has($cache_key))
+    {
+      $aData = Cache::get($cache_key);
+    }
+    else
+    {
+      $oRequest = Bandwidth::select('request_time', DB::raw('avg(time_taken) as avg_time_taken'))
       ->whereHas('domain', function($q) use ($domain_name)
       {
         $q->where('domain', $domain_name);
@@ -15,76 +84,37 @@ class OvertimeController extends BaseController {
       ->groupBy(DB::raw('YEAR(request_time), MONTH(request_time), DAY(request_time)'))
       ->orderBy('request_time');
 
-    if(!empty($subdomain_name)) {
-      $oBandwidth = $oBandwidth->whereHas('domain', function($q) use ($subdomain_name)
-      {
-        $q->where('subdomain', $subdomain_name);
-      });
+      if(!empty($subdomain_name)) {
+        $oRequest = $oRequest->whereHas('domain', function($q) use ($subdomain_name)
+        {
+          $q->where('subdomain', $subdomain_name);
+        });
+      }
+
+      $aRequest = $oRequest->get();
+
+      $aAvg = array(
+        'name' => 'Average',
+        'data' => array(),
+        'tooltip' => array(
+          'valueDecimals' => 2,
+          'valueSuffix' => ' Sec'
+        )
+      );
+
+      foreach($aRequest as $aRow) {
+        $date = new DateTime($aRow['request_time']);
+
+        $aAvg['data'][] = array((int)$date->format('U')*1000, (float)$aRow['avg_time_taken']/1000);
+      }
+
+      $aData = array($aAvg);
+
+      $expiresAt = new DateTime();
+      $expiresAt = $expiresAt->modify('+1 Hour');
+      Cache::put($cache_key, $aData, $expiresAt);
     }
 
-    $aBandwidth = $oBandwidth->get();
-
-    $aReceived = array(
-      'name' => 'Received',
-      'data' => array(),
-      'tooltip' => array(
-        'valueDecimals' => 2,
-        'valueSuffix' => ' KB'
-      )
-    );
-    $aSent = array(
-      'name' => 'Sent',
-      'data' => array(),
-      'tooltip' => array(
-        'valueDecimals' => 2,
-        'valueSuffix' => ' KB'
-      )
-    );
-
-    foreach($aBandwidth as $aRow) {
-      $date = new DateTime($aRow['request_time']);
-
-      $aReceived['data'][] = array((int)$date->format('U')*1000, (float)number_format($aRow['bytes_received'] / 1024, 2));
-      $aSent['data'][] = array((int)$date->format('U')*1000, (float)number_format($aRow['bytes_sent'] / 1024, 2));
-    }
-
-    return Response::json(array($aReceived, $aSent), 200);
-  }
-
-  public function time_taken($domain_name, $subdomain_name = null)
-  {
-    $oRequest = Bandwidth::select('request_time', DB::raw('avg(time_taken) as avg_time_taken'))
-    ->whereHas('domain', function($q) use ($domain_name)
-    {
-      $q->where('domain', $domain_name);
-    })
-    ->groupBy(DB::raw('YEAR(request_time), MONTH(request_time), DAY(request_time)'))
-    ->orderBy('request_time');
-
-    if(!empty($subdomain_name)) {
-      $oRequest = $oRequest->whereHas('domain', function($q) use ($subdomain_name)
-      {
-        $q->where('subdomain', $subdomain_name);
-      });
-    }
-
-    $aRequest = $oRequest->get();
-
-    $aAvg = array(
-      'name' => 'Average',
-      'data' => array(),
-      'tooltip' => array(
-        'valueDecimals' => 2,
-        'valueSuffix' => ' Sec'
-      )
-    );
-
-    foreach($aRequest as $aRow) {
-      $date = new DateTime($aRow['request_time']);
-
-      $aAvg['data'][] = array((int)$date->format('U')*1000, (float)$aRow['avg_time_taken']/1000);
-    }
-
-    return Response::json(array($aAvg), 200);
+    return Response::json($aData, 200);
   }
 }
