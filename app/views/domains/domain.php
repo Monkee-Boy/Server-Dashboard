d36d5444d3a886ef25ea48d442eb787c54f803ef
John Hoover <defvayne23@gmail.com>
2015-02-02 20:22:38 -0600

    Add tab layout to domain and subdomain pages.

--- app/views/domains/domain.php
+++ app/views/domains/domain.php
@@ -14,34 +14,50 @@
   <li class="current"><?= $domain_name ?></li>
 </ul>
 
-<div class="row">
-  <div class="large-12 columns">
-    <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_bandwidth_domain', array('domain_name'=>$domain_name)) ?>" data-chart-title="Bandwidth" data-yaxis-title="Kilobytes"></div>
+<ul class="tabs" data-tab>
+  <li class="tab-title active"><a href="#panel1">Over Time</a></li>
+  <li class="tab-title"><a href="#panel2">Subdomains</a></li>
+  <li class="tab-title"><a href="#panel3">Query Builder</a></li>
+</ul>
+
+<hr>
+
+<div class="tabs-content">
+  <div class="content active" id="panel1">
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_bandwidth_domain', array('domain_name'=>$domain_name)) ?>" data-chart-title="Bandwidth" data-yaxis-title="Kilobytes"></div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_time_domain', array('domain_name'=>$domain_name)) ?>" data-chart-title="Request Time" data-yaxis-title="Seconds"></div>
+      </div>
+    </div>
+  </div>
+  <div class="content" id="panel2">
+    <table style="width: 100%;">
+      <thead>
+        <tr>
+          <th>Subdomain</th>
+          <th>Bandwidth <span data-tooltip aria-haspopup="true" class="has-tip" title="Sum of bandwidth sent and received in the last 30 days.">?</span></th>
+          <th>Storage</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <?php foreach($subdomains as $subdomain) { ?>
+          <tr>
+            <td><?= link_to_route('subdomain', $subdomain->subdomain, array('domain_name' =>$subdomain->domain, 'subdomain_name' => $subdomain->subdomain)); ?></td>
+            <td><?= $subdomain->bandwidth ?></td>
+            <td></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+  <div class="content" id="panel3">
+    #TODO: Add query builder.
   </div>
 </div>
-
-<div class="row">
-  <div class="large-12 columns">
-    <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_time_domain', array('domain_name'=>$domain_name)) ?>" data-chart-title="Request Time" data-yaxis-title="Seconds"></div>
-  </div>
-</div>
-
-<table style="width: 100%;">
-  <thead>
-    <tr>
-      <th>Subdomain</th>
-      <th>Bandwidth <span data-tooltip aria-haspopup="true" class="has-tip" title="Sum of bandwidth sent and received in the last 30 days.">?</span></th>
-      <th>Storage</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <?php foreach($subdomains as $subdomain) { ?>
-      <tr>
-        <td><?= link_to_route('subdomain', $subdomain->subdomain, array('domain_name' =>$subdomain->domain, 'subdomain_name' => $subdomain->subdomain)); ?></td>
-        <td><?= $subdomain->bandwidth ?></td>
-        <td></td>
-      </tr>
-    <?php } ?>
-  </tbody>
-</table>
--- app/views/domains/subdomain.php
+++ app/views/domains/subdomain.php
@@ -15,14 +15,28 @@
   <li class="current"><?= $subdomain->subdomain ?></li>
 </ul>
 
-<div class="row">
-  <div class="large-12 columns">
-    <div class="chartStock" width="100%" data-chart-url="<?= route('chart_bandwidth_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Bandwidth" data-chart-series="Bandwidth"></div>
-  </div>
-</div>
+<ul class="tabs" data-tab>
+  <li class="tab-title active"><a href="#panel1">Over Time</a></li>
+  <li class="tab-title"><a href="#panel3">Query Builder</a></li>
+</ul>
 
-<div class="row">
-  <div class="large-12 columns">
-    <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_time_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Request Time" data-yaxis-title="Seconds"></div>
+<hr>
+
+<div class="tabs-content">
+  <div class="content active" id="panel1">
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="chartStock" width="100%" data-chart-url="<?= route('chart_bandwidth_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Bandwidth" data-chart-series="Bandwidth"></div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_time_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Request Time" data-yaxis-title="Seconds"></div>
+      </div>
+    </div>
+  </div>
+  <div class="content" id="panel2">
+    #TODO: Add query builder.
   </div>
 </div>
