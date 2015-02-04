fe689a2c549c5ea22b8e2389a5084ae4f82faa4f
John Hoover <defvayne23@gmail.com>
2015-02-03 22:14:43 -0600

    Keep tab state when changing tabs.

--- app/views/domains/domain.php
+++ app/views/domains/domain.php
@@ -14,7 +14,7 @@
   <li class="current"><?= $domain_name ?></li>
 </ul>
 
-<ul class="tabs" data-tab>
+<ul class="tabs" data-tab data-options="deep_linking:true" data-options="scroll_to_content: false">
   <li class="tab-title active"><a href="#panel1">Over Time</a></li>
   <li class="tab-title"><a href="#panel2">Top</a></li>
   <li class="tab-title"><a href="#panel3">Subdomains</a></li>
