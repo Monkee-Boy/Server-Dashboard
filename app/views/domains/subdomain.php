8e5167b79f15e9cd2a00403bcf3542d530ee0598
John Hoover <defvayne23@gmail.com>
2015-04-05 21:17:18 -0500

    Change 'Top' tab to 'Overview'.
    Rename 'Over Time' to 'Bandwidth'.

--- app/views/domains/subdomain.php
+++ app/views/domains/subdomain.php
@@ -16,9 +16,9 @@
 </ul>
 
 <ul class="tabs" data-tab data-options="deep_linking:true;scroll_to_content:false">
-  <li class="tab-title active"><a href="#panel1">Over Time</a></li>
-  <li class="tab-title"><a href="#panel2">Storage</a></li>
-  <li class="tab-title"><a href="#panel3">Top</a></li>
+  <li class="tab-title active"><a href="#panel1">Overview</a></li>
+  <li class="tab-title"><a href="#panel2">Bandwidth</a></li>
+  <li class="tab-title"><a href="#panel3">Storage</a></li>
   <li class="tab-title"><a href="#panel4">Query Builder</a></li>
 </ul>
 
@@ -27,33 +27,8 @@
 <div class="tabs-content">
   <div class="content active" id="panel1">
     <div class="row">
-      <div class="large-12 columns">
-        <div class="chartStock" width="100%" data-chart-url="<?= route('chart_bandwidth_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Bandwidth" data-chart-series="Bandwidth"></div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="large-12 columns">
-        <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_time_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Request Time" data-yaxis-title="Seconds"></div>
-      </div>
-    </div>
-  </div>
-
-  <div class="content" id="panel2">
-    <div class="row">
       <div class="large-6 columns">
-        <div class="chartPie" data-chart-url="<?= route('chart_storage_total', ['domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain]); ?>" data-chart-title="Storage Breakdown" data-chart-type="storage" style="width: 100%; height: 400px;"></div>
-      </div> <!-- /.large-6.columns -->
-
-      <div class="large-6 columns">
-
-      </div> <!-- /.large-6.columns -->
-    </div> <!-- /.row -->
-  </div> <!-- /.content -->
-
-  <div class="content" id="panel3">
-    <div class="row">
-      <div class="large-6 columns">
+        <!-- TODO: Show bandwidth this month and last -->
         <h5>Top Referers <span data-tooltip aria-haspopup="true" class="has-tip" title="Top 10 referers in the last 30 days.">?</span></h5>
         <table class="chartTable" data-chart-url="<?= route('chart_top_referers', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" style="width: 100%;">
           <thead>
@@ -67,6 +42,7 @@
       </div> <!-- /.large-6.columns -->
 
       <div class="large-6 columns">
+        <!-- TODO: Show total storage -->
         <h5>Top Pages <span data-tooltip aria-haspopup="true" class="has-tip" title="Top 10 pages requested in the last 30 days.">?</span></h5>
         <table class="chartTable" data-chart-url="<?= route('chart_top_pages', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" style="width: 100%;">
           <thead>
@@ -79,7 +55,34 @@
         </table>
       </div> <!-- /.large-6.columns -->
     </div> <!-- /.row -->
+  </div> <!-- /.content.active -->
+
+  <div class="content" id="panel2">
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="chartStock" width="100%" data-chart-url="<?= route('chart_bandwidth_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Bandwidth" data-chart-series="Bandwidth"></div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="chartStock" width="100%" height="400px" data-chart-url="<?= route('chart_time_domain', array('domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain)) ?>" data-chart-title="Request Time" data-yaxis-title="Seconds"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="content" id="panel3">
+    <div class="row">
+      <div class="large-6 columns">
+        <div class="chartPie" data-chart-url="<?= route('chart_storage_total', ['domain_name'=>$subdomain->domain, 'subdomain_name'=>$subdomain->subdomain]); ?>" data-chart-title="Storage Breakdown" data-chart-type="storage" style="width: 100%; height: 400px;"></div>
+      </div> <!-- /.large-6.columns -->
+
+      <div class="large-6 columns">
+
+      </div> <!-- /.large-6.columns -->
+    </div> <!-- /.row -->
   </div> <!-- /.content -->
+
   <div class="content" id="panel4">
     #TODO: Add query builder.
   </div>
